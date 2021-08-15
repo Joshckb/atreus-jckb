a0e5a9f8a3b00873a0da4d35996f4e71f8a31255
Josue Mavarez <josuemavarez@gmail.com>
2021-08-15 20:04:06 -0300

    fix: update dvorak layer position to use mods

--- keymap.c
+++ keymap.c
@@ -2,15 +2,17 @@
 
 enum layer_number {
   _QW = 0,
+  _DV,
   _LW,
   _RS,
   _AD,
-  _DV
 };
 
 enum custom_keycode {
     ALT_TAB = SAFE_RANGE,
     CTL_TAB,
+    QWERTY,
+    DVORAK,
 };
 
 bool is_alt_tab_active = false;
@@ -25,6 +27,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_A,           KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
     LSFT_T(KC_Z),   KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
     KC_GESC,        KC_TAB,  KC_LALT, KC_LGUI, MO(_RS),  LCTL_T(KC_SPC), KC_ENT,  MO(_LW), KC_MINS, KC_RALT, KC_QUOT, KC_BSPC
+  ),
+
+  [_DV] = LAYOUT( /* Dvorak */
+    KC_SCLN,        KC_COMM, KC_DOT,  KC_P,    KC_Y,                                KC_F,    KC_G,    KC_C,    KC_R,    KC_BSLS,
+    KC_A,           KC_O,    KC_E,    KC_U,    KC_I,                                KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
+    LSFT_T(KC_Z),   KC_Q,    KC_J,    KC_K,    KC_X,                                KC_B,    KC_M,    KC_W,    KC_V,    RSFT_T(KC_Z),
+    KC_GESC,        KC_TAB,  KC_LALT, KC_LGUI, MO(_RS),  LCTL_T(KC_SPC), KC_ENT,    MO(_LW), KC_RALT, KC_MINS, KC_SLSH, KC_BSPC
   ),
 
   [_RS] = LAYOUT( /* Raise */
@@ -42,18 +51,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_AD] = LAYOUT( /* Adjust */
-    TG(_AD),   KC_NO,    KC_NO,    DF(_DV), DF(_QW),                  KC_NLCK,    KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
+    TG(_AD),   KC_NO,    KC_NO,    DVORAK,  QWERTY,                   KC_NLCK,    KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
     KC_NO,     KC_NO,    KC_NO,    KC_NO,   KC_NO,                    KC_PAST,    KC_P4,    KC_P5,    KC_P6,    KC_PMNS,
     KC_NO,     KC_NO,    KC_NO,    KC_NO,   KC_NO,                    KC_PSLS,    KC_P1,    KC_P2,    KC_P3,    KC_PEQL,
     RESET,     KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_NO, KC_NO,     KC_ENTER,   KC_RALT,  KC_MINS,  KC_P0,    KC_BSPC
   ),
 
-  [_DV] = LAYOUT( /* Dvorak */
-    KC_SCLN,        KC_COMM, KC_DOT,  KC_P,    KC_Y,                                KC_F,    KC_G,    KC_C,    KC_R,    KC_BSLS,
-    KC_A,           KC_O,    KC_E,    KC_U,    KC_I,                                KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
-    LSFT_T(KC_Z),   KC_Q,    KC_J,    KC_K,    KC_X,                                KC_B,    KC_M,    KC_W,    KC_V,    RSFT_T(KC_Z),
-    KC_GESC,        KC_TAB,  KC_LALT, KC_LGUI, MO(_RS),  LCTL_T(KC_SPC), KC_ENT,    MO(_LW), KC_RALT, KC_MINS, KC_SLSH, KC_BSPC
-  )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -82,6 +85,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_TAB);
             }
             break;
+        case QWERTY:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_QW);
+            }
+        break;
+        case DVORAK:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_DV);
+            }
+        break;
     }
 
     return true;
